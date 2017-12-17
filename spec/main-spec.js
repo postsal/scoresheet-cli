c7ab6276db493bc22a805e1fea612702c3bb306d
postsal <1072841098@qq.com>
2017-12-17 08:23:18 +0000

    finish

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -11,5 +11,21 @@ describe('main()', () => {
 3. 退出
 请输入你的选择（1～3）：`);
     });
+    it('should display the added student indormation',function(){
+        sinon.spy(console, 'log');
+        let input = "hujian,2015,han,1,数学:80,语文:80,英语:80,编程:80";
+        main.stdin1(input);
+        expect(console.log).toEqual(`
+        { name: 'hujian',
+          code: '2015',
+          nation: 'han',
+          klass: '1',
+          scores:
+           [ { course: '数学', score: 80 },
+             { course: '语文', score: 80 },
+             { course: '英语', score: 80 },
+             { course: '编程', score: 80 } ],
+          sumScore: 320 }`);
+    });
 
 });
