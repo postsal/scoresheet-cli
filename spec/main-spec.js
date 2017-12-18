fe2c9fceb0545718eb3ac6740012374037443760
postsal <1072841098@qq.com>
2017-12-18 06:56:28 +0000

    finish

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,15 +2,6 @@ let sinon = require("sinon");
 let main = require("../lib/main");
 
 describe('main()', () => {
-
-    it('should display main menu once started', () => {
-        sinon.spy(console, 'log');
-        main.main();
-        expect(console.log.args.join()).toBe(`1. 添加学生
-2. 生成成绩单
-3. 退出
-请输入你的选择（1～3）：`);
-    });
     it('should display the added student indormation',function(){
         sinon.spy(console, 'log');
         let input = "hujian,2015,han,1,数学:80,语文:80,英语:80,编程:80";
